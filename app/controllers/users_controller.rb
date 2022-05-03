f13cb0d9ecdfd48b61aa6324ae70419508b60a1c
Mylene Desroses <mylene.desroses@gmail.com>
2022-05-03 16:39:53 -0400

    fix error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+
   def create
     @user = User.new(user_params)
 
-    if user.save
+    if @user.save
       session = JWTSessions::Session.new(payload: payload, refresh_by_access_allowed: true)
       render json: session.login
     else
-      render json: { error: user.errors.full_messages.join(' ') }, status: :unprocessable_entity
+      render json: { error: @user.errors.full_messages.join(' ') }, status: :unprocessable_entity
     end
   end
 
